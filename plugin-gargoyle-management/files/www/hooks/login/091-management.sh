976122c3e420fd12fd6149e2015f8d0007fac6f8
Cezary Jackiewicz <cezary.jackiewicz@gmail.com>
2014-12-23 06:50:14 +0100

    Plugin management: typo

--- plugin-gargoyle-management/files/www/hooks/login/091-management.sh
+++ plugin-gargoyle-management/files/www/hooks/login/091-management.sh
@@ -2,7 +2,7 @@
 
 <fieldset id="management">
 	<legend class="sectionheader">Zarządzanie</legend>
-	<center><input type='button' value='Uruchom ponowne' id="reboot_button" class="big_button" onclick='reboot()' /></center>
+	<center><input type='button' value='Uruchom ponownie' id="reboot_button" class="big_button" onclick='reboot()' /></center>
 	<div class="internal_divider"></div>
 	<center><input type='button' value='Restart WAN' id="reconnect_button" class="big_button" onclick='reconnect()' /></center>
 </fieldset>
