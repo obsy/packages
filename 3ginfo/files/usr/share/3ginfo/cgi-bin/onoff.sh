9128fc2582b1095c014a31bd18e59888529e2a0b
Cezary Jackiewicz <cezary.jackiewicz@gmail.com>
2016-02-22 20:38:36 +0100

    3ginfo: fix on/off script

--- 3ginfo/files/usr/share/3ginfo/cgi-bin/onoff.sh
+++ 3ginfo/files/usr/share/3ginfo/cgi-bin/onoff.sh
@@ -1,28 +1,13 @@
 #!/bin/sh
 
-DEVICE=$(uci -q get 3ginfo.@3ginfo[0].device)
+SEC=$(uci -q get 3ginfo.@3ginfo[0].network)
+[ -z "$SEC" ] && exit 0
 
-for DEV in /sys/class/tty/${DEVICE##/*/}/device/driver/tty*; do
-	SEC=$(uci show network | grep "/dev/"${DEV##/*/} | cut -f2 -d.)
-	if [ ! -z $SEC ]; then
-		break
-	fi
-done
-if [ -z $SEC ]; then
-	SEC=$(uci show network | grep ${DEVICE%%[0-9]} | cut -f2 -d.)
-fi
-
-if [ ${DEVICE%%[0-9]} = "/dev/ttyHS" ]; then
-	IFACE="hso0"
-else
-	IFACE="3g-"$SEC
-fi
-
-if ifconfig $IFACE 2>/dev/null | grep -q inet; then
+UP=$(ifstatus wan | grep "\"up\": true")
+if [ -n "$UP" ]; then
 	ifdown $SEC > /dev/null 2>&1
 else
 	ifup $SEC > /dev/null 2>&1
-	sleep 2
 fi
 
 sleep 3
