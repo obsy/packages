09bedff858c0f19f763eb3d64808c80cc3795144
Cezary Jackiewicz <cezary.jackiewicz@gmail.com>
2016-03-02 07:32:51 +0100

    3ginfo: disbale onoff script if button disabled

--- 3ginfo/files/usr/share/3ginfo/cgi-bin/onoff.sh
+++ 3ginfo/files/usr/share/3ginfo/cgi-bin/onoff.sh
@@ -3,6 +3,9 @@
 SEC=$(uci -q get 3ginfo.@3ginfo[0].network)
 [ -z "$SEC" ] && exit 0
 
+ALLOW=$(uci -q get 3ginfo.@3ginfo[0].connect_button)
+[ "x$ALLOW" = "x0" ] && exit 0
+
 UP=$(ifstatus $SEC | grep "\"up\": true")
 if [ -n "$UP" ]; then
 	ifdown $SEC > /dev/null 2>&1
