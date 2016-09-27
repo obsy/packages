5f395d4ceebfaba1ceacff75a3447974b07d737e
Cezary Jackiewicz <cezary.jackiewicz@gmail.com>
2016-09-27 18:37:29 +0200

    3ginfo: replace btsinfo.pl by opencellid

--- 3ginfo/files-text/usr/share/3ginfo/cgi-bin/3ginfo.sh
+++ 3ginfo/files-text/usr/share/3ginfo/cgi-bin/3ginfo.sh
@@ -370,31 +370,32 @@ CID=$(echo "$O" | awk -F[,] '/\'$CREG'/ {printf "%s", toupper($4)}' | sed 's/[^A
 if [ "x$CID" != "x" ]; then
 	CID_NUM=$(printf %d 0x$CID)
 
-	if [ ${#CID} -le 4 ]; then
-		if [ $FORMAT -eq 0 ]; then
-			case $COPS_NUM in
-				26001*) CID="<a href=\"http://btsearch.pl/szukaj.php?search="$CID"h\&amp;siec=3\&amp;mode=adv\">$CID</a>";;
-				26002*) CID="<a href=\"http://btsearch.pl/szukaj.php?search="$CID"h\&amp;siec=1\&amp;mode=adv\">$CID</a>";;
-				26003*) CID="<a href=\"http://btsearch.pl/szukaj.php?search="$CID"h\&amp;siec=2\&amp;mode=adv\">$CID</a>";;
-				26006*) CID="<a href=\"http://btsearch.pl/szukaj.php?search="$CID"h\&amp;siec=4\&amp;mode=adv\">$CID</a>";;
-				26016*) CID="<a href=\"http://btsearch.pl/szukaj.php?search="$CID"h\&amp;siec=7\&amp;mode=adv\">$CID</a>";;
-				26017*) CID="<a href=\"http://btsearch.pl/szukaj.php?search="$CID"h\&amp;siec=8\&amp;mode=adv\">$CID</a>";;
-			esac
+	if [ $FORMAT -eq 0 ]; then
+		T="$LAC_NUM"
+		[ "x$LAC" = "xFFFE" -a "x$TAC" != "x-" ] && T="$TAC_NUM"
+		if [ "x$LAC" != "x-" -a "x$T" != "x-" -a "x$COPS_MCC" != "x-" -a "x$COPS_MNC" != "x-" ]; then
+			CID="<a href=\"http://opencellid.org/#action=filters.measuresOfGivenBaseStation\&mcc=$COPS_MCC\&mnc=$COPS_MNC\&lac=$T\&cellid=$CID_NUM\&lang=$LANG\">$CID</a>"
 		fi
+	fi
 
-		CLF=$(uci -q get 3ginfo.@3ginfo[0].clf)
-		if [ -e "$CLF" ]; then
-			PAT="xxx"
-			[ "x$CID_NUM" != "x-" -a "x$LAC_NUM" != "x-" ] && PAT="^$COPS_NUM;0x"$(printf %04X $CID_NUM)";0x"$(printf %04X $LAC_NUM)";"
-			is_gz=$(dd if="$CLF" bs=1 count=2 2>/dev/null | hexdump -v -e '1/1 "%02x"')
-			if [ "x$is_gz" = "x1f8b" ] ; then
-				BTSINFO="<a href=\"http://maps.google.pl/?t=k\&z=17\&q="$(zcat "$CLF" | awk -F";" '/'$PAT'/ {printf $5","$6}')"\">"$(zcat "$CLF" | awk -F";" '/'$PAT'/ {gsub(/\!/,"\\!");print $8}')"</a>"
-			else
-				BTSINFO="<a href=\"http://maps.google.pl/?t=k\&z=17\&q="$(awk -F";" '/'$PAT'/ {printf $5","$6}' "$CLF")"\">"$(awk -F";" '/'$PAT'/ {gsub(/\!/,"\\!");print $8}' "$CLF")"</a>"
-			fi
-			if [ $FORMAT -eq 2 ]; then
-				BTSINFO=$(echo "$BTSINFO" | sed 's!<a.*>\(.*\)</a>!\1!g')
-			fi
+	if [ ${#CID} -gt 4 ]; then
+		T=$(echo "$CID" | awk '{print substr($1,length(substr($1,1,length($1)-4))+1)}')
+	else
+		T=$CID
+	fi
+
+	CLF=$(uci -q get 3ginfo.@3ginfo[0].clf)
+	if [ -e "$CLF" ]; then
+		PAT="xxx"
+		[ "x$T" != "x-" -a "x$LAC_NUM" != "x-" ] && PAT="^$COPS_NUM;0x"$(printf %04X 0x$T)";0x"$(printf %04X $LAC_NUM)";"
+		is_gz=$(dd if="$CLF" bs=1 count=2 2>/dev/null | hexdump -v -e '1/1 "%02x"')
+		if [ "x$is_gz" = "x1f8b" ] ; then
+			BTSINFO="<a href=\"http://maps.google.pl/?t=k\&z=17\&q="$(zcat "$CLF" | awk -F";" '/'$PAT'/ {printf $5","$6}')"\">"$(zcat "$CLF" | awk -F";" '/'$PAT'/ {gsub(/\!/,"\\!");print $8}')"</a>"
+		else
+			BTSINFO="<a href=\"http://maps.google.pl/?t=k\&z=17\&q="$(awk -F";" '/'$PAT'/ {printf $5","$6}' "$CLF")"\">"$(awk -F";" '/'$PAT'/ {gsub(/\!/,"\\!");print $8}' "$CLF")"</a>"
+		fi
+		if [ $FORMAT -eq 2 ]; then
+			BTSINFO=$(echo "$BTSINFO" | sed 's!<a.*>\(.*\)</a>!\1!g')
 		fi
 	fi
 else
