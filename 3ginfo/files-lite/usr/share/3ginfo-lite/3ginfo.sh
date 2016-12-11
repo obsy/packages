c1b2f325f3232437fc4352465d117dede2622f1b
Cezary Jackiewicz <cezary.jackiewicz@gmail.com>
2016-12-11 15:50:44 +0100

    3ginfo: missing files

---
+++ 3ginfo/files-lite/usr/share/3ginfo-lite/3ginfo.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+#
+# (c) 2010-2016 Cezary Jackiewicz <cezary@eko.one.pl>
+#
+
+RES="/usr/share/3ginfo-lite"
+
+touch /tmp/modem
+DEVICE=$(cat /tmp/modem)
+
+if [ "x$DEVICE" = "x" ]; then
+	devices=$(ls /dev/ttyUSB* /dev/cdc-wdm* /dev/ttyACM* /dev/ttyHS* 2>/dev/null | sort -r)
+	for d in $devices; do
+		DEVICE=$d gcom -s $RES/probeport.gcom > /dev/null 2>&1
+		if [ $? = 0 ]; then
+			echo "$d" > /tmp/modem
+			break
+		fi
+	done
+	DEVICE=$(cat /tmp/modem)
+fi
+
+if [ "x$DEVICE" = "x" ]; then
+	echo "{}"
+	exit 0
+fi
+
+O=$(gcom -d $DEVICE -s $RES/3ginfo.gcom 2>/dev/null)
+
+# CSQ
+CSQ=$(echo "$O" | awk -F[,\ ] '/^\+CSQ/ {print $2}')
+
+[ "x$CSQ" = "x" ] && CSQ=-1
+if [ $CSQ -ge 0 -a $CSQ -le 31 ]; then
+	CSQ_PER=$(($CSQ * 100/31))
+else
+	CSQ="-"
+	CSQ_PER="0"
+fi
+
+# COPS
+COPS_NUM=$(echo "$O" | awk -F[\"] '/^\+COPS: .,2/ {print $2}')
+if [ "x$COPS_NUM" = "x" ]; then
+	COPS_NUM="-"
+	COPS_MCC="-"
+	COPS_MNC="-"
+else
+	COPS_MCC=${COPS_NUM:0:3}
+	COPS_MNC=${COPS_NUM:3:3}
+	COPS=$(awk -F[\;] '/'$COPS_NUM'/ {print $2}' $RES/mccmnc.dat)
+	[ "x$COPS" = "x" ] && COPS="-"
+fi
+[ "x$COPS" = "x" ] && COPS=$COPS_NUM
+
+# MODE
+TECH=$(echo "$O" | awk -F[,] '/^\+COPS/ {print $4}')
+case "$TECH" in
+	2*) MODE="UMTS";;
+	3*) MODE="EDGE";;
+	4*) MODE="HSDPA";;
+	5*) MODE="HSUPA";;
+	6*) MODE="HSPA";;
+	7*) MODE="LTE";;
+	 *) MODE="-";;
+esac
+
+echo "{\"signal\":\"$CSQ_PER\",\"operator\":\"$COPS\",\"mode\":\"$MODE\"}"
+
+exit 0
