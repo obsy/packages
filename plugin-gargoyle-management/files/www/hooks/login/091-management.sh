1466f3d8a72318fd49b154ebe395d3bcd6b011ac
Cezary Jackiewicz <cezary@eko.one.pl>
2017-10-16 08:52:45 +0200

    plugin management: update gui

--- plugin-gargoyle-management/files/www/hooks/login/091-management.sh
+++ plugin-gargoyle-management/files/www/hooks/login/091-management.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/haserl
 
-<fieldset id="management">
-	<legend class="sectionheader">Zarządzanie</legend>
-	<center><input type='button' value='Uruchom ponownie' id="reboot_button" class="big_button" onclick='reboot()' /></center>
-	<div class="internal_divider"></div>
-	<center><input type='button' value='Restart WAN' id="reconnect_button" class="big_button" onclick='reconnect()' /></center>
-</fieldset>
+<div id="management" class="row">
+	<div class="col-lg-4">
+		<div class="panel panel-info">
+
+		<div class="panel-heading">
+			<h3 class="panel-title">Zarządzanie</h3>
+		</div>
+
+		<div class="panel-body">
+
+		<button id="reboot_button" class="btn btn-default" onclick='reboot()'>Uruchom ponownie</button>
+		<div class="internal_divider"></div>
+		<button id="reconnect_button" class="btn btn-default" onclick='reconnect()'>Restart WAN</button>
+
+		</div>
+
+		</div>
+	</div>
+</div>
 
 <iframe id="reboot_test" onload="reloadPage()" style="display:none" ></iframe>
