80b22a1eb7df06c3220a4565ea922badf47f7ed7
Cezary Jackiewicz <cezary@eko.one.pl>
2018-03-27 22:52:31 +0200

    easyconfig: add Yandex.DNS

--- easyconfig/files/www/easyconfig/dns.js
+++ easyconfig/files/www/easyconfig/dns.js
@@ -8,5 +8,8 @@ var dns=[
 {"ip": ["9.9.9.9"], "name": "Quad 9", "url": "https://www.quad9.net/"},
 {"ip": ["176.103.130.130", "176.103.130.131"], "name": "AdGuard DNS Default", "url": "https://adguard.com/en/adguard-dns/overview.html"},
 {"ip": ["176.103.130.132", "176.103.130.134"], "name": "AdGuard DNS Family protection", "url": "https://adguard.com/en/adguard-dns/overview.html"},
-{"ip": ["8.26.56.26", "8.20.247.20"], "name": "Comodo Secure DNS", "url": "https://www.comodo.com/secure-dns/"}
+{"ip": ["8.26.56.26", "8.20.247.20"], "name": "Comodo Secure DNS", "url": "https://www.comodo.com/secure-dns/"},
+{"ip": ["77.88.8.8", "77.88.8.1"], "name": "Yandex.DNS Basic", "url": "https://dns.yandex.com/"},
+{"ip": ["77.88.8.88", "77.88.8.2"], "name": "Yandex.DNS Safe", "url": "https://dns.yandex.com/"},
+{"ip": ["77.88.8.7", "77.88.8.3"], "name": "Yandex.DNS Family", "url": "https://dns.yandex.com/"}
 ];
