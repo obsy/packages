05d380ddaa0e765fec2ebb9a033aed71c67043e7
Cezary Jackiewicz <cezary@eko.one.pl>
2019-04-14 10:23:24 +0200

    easyconfig: clients: logs: fix log order

--- easyconfig/files/usr/bin/easyconfig_wlanlogs.sh
+++ easyconfig/files/usr/bin/easyconfig_wlanlogs.sh
@@ -4,11 +4,11 @@ LOGS=/tmp/easyconfig_wlanlogs.txt
 T=$(mktemp)
 
 logread -e "DHCPACK(" >> $T
-logread -e "AP-STA-" >> $T
+logread -e "AP-STA-.*CONNECTED" >> $T
 if [ -s "$T" ]; then
 	touch $LOGS.gz
 	zcat $LOGS.gz >> $T 2>/dev/null
-	sort $T | uniq > $LOGS
+	sed 's/daemon.notice/daemon.1/g;s/daemon.info/daemon.2/g' $T | sort | uniq > $LOGS
 	gzip -f $LOGS
 fi
 rm $T
