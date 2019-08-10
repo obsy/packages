0bc783fc0209427a56e4c1ffd10ee378ad09ff73
Cezary Jackiewicz <cezary@eko.one.pl>
2019-08-10 13:21:01 +0200

    easyconfig: clients: logs: add extra timestamp to ensure proper timeline

--- easyconfig/files/usr/bin/easyconfig_wlanlogs.sh
+++ easyconfig/files/usr/bin/easyconfig_wlanlogs.sh
@@ -3,11 +3,11 @@
 LOGS=/tmp/easyconfig_wlanlogs.txt
 T=$(mktemp)
 
-logread -e "AP-STA-.*CONNECTED\|DHCPACK(" >> $T
+logread -t -e "AP-STA-.*CONNECTED\|DHCPACK(" >> $T
 if [ -s "$T" ]; then
 	touch $LOGS.gz
 	zcat $LOGS.gz >> $T 2>/dev/null
-	sed 's/daemon.notice/daemon.1/g;s/daemon.info/daemon.2/g' $T | sort | uniq > $LOGS
+	sort $T | uniq > $LOGS
 	gzip -f $LOGS
 fi
 rm $T
