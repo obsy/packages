070fb8bf35547fbd4ee44d028d2654e472f02901
Cezary Jackiewicz <cezary@eko.one.pl>
2020-03-06 23:25:02 +0100

    easyconfig: check crontabs existance

--- easyconfig/files/etc/uci-defaults/zz-easyconfig.sh
+++ easyconfig/files/etc/uci-defaults/zz-easyconfig.sh
@@ -2,6 +2,6 @@
 [ ! -e /www/index.html ] && ln -s /www/easyconfig.html /www/index.html
 uci set dhcp.@dnsmasq[0].logqueries='1'
 uci commit dhcp
-sed -i "/easyconfig_wlanlogs/d" /etc/crontabs/root
+[ -e /etc/crontabs/root ] && sed -i "/easyconfig_wlanlogs/d" /etc/crontabs/root
 echo "*/1 * * * * /usr/bin/easyconfig_wlanlogs.sh" >> /etc/crontabs/root
 exit 0
