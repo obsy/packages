e3c75877b8106792ae0306554b11c12148dca38c
Cezary Jackiewicz <cezary@eko.one.pl>
2020-04-03 18:12:35 +0200

    sysinfo: show all interfaces

--- sysinfo/files/sbin/sysinfo.sh
+++ sysinfo/files/sbin/sysinfo.sh
@@ -25,16 +25,16 @@ D=$(expr $U / 60 / 60 / 24)
 H=$(expr $U / 60 / 60 % 24)
 M=$(expr $U / 60 % 60)
 S=$(expr $U % 60)
-U=$(printf "%dd, %02d:%02d:%02d" $D $H $M $S)
+U=$(printf "%dd, %02d:%02d:%02d" "$D" "$H" "$M" "$S")
 
 L=$(awk '{ print $1" "$2" "$3}' /proc/loadavg)
 
 RFS=$(df /overlay 2>/dev/null | awk '/overlay/ {printf "%.0f:%.0f:%s", $4*1024, $2*1024, $5}')
 [ -z "$RFS" ] && RFS=$(df / 2>/dev/null | awk '/dev\/root/ {printf "%.0f:%.0f:%s", $4*1024, $2*1024, $5}')
 if [ -n "$RFS" ]; then
-	a1=$(echo $RFS | cut -f1 -d:)
-	a2=$(echo $RFS | cut -f2 -d:)
-	a3=$(echo $RFS | cut -f3 -d:)
+	a1=$(echo "$RFS" | cut -f1 -d:)
+	a2=$(echo "$RFS" | cut -f2 -d:)
+	a3=$(echo "$RFS" | cut -f3 -d:)
 	RFS="total: "$(hr $a2)", free: "$(hr $a1)", used: "$a3
 fi
 
@@ -42,63 +42,67 @@ total_mem="$(awk '/^MemTotal:/ {print $2*1024}' /proc/meminfo)"
 buffers_mem="$(awk '/^Buffers:/ {print $2*1024}' /proc/meminfo)"
 cached_mem="$(awk '/^Cached:/ {print $2*1024}' /proc/meminfo)"
 free_mem="$(awk '/^MemFree:/ {print $2*1024}' /proc/meminfo)"
-free_mem="$(( ${free_mem} + ${buffers_mem} + ${cached_mem} ))"
+free_mem="$((free_mem + buffers_mem + cached_mem))"
 MEM=$(echo "total: "$(hr $total_mem)", free: "$(hr $free_mem)", used: "$(( (total_mem - free_mem) * 100 / total_mem))"%")
 
-LAN=$(uci -q get network.lan.ipaddr)
-[ -e /tmp/dhcp.leases ] && LAN="$LAN, leases: "$(awk 'END {print NR}' /tmp/dhcp.leases)
+printf " | %-""$LINE""s |\n" "Machine: $MACH"
+printf " | %-""$LINE""s |\n" "Uptime: $U"
+printf " | %-""$LINE""s |\n" "Load: $L"
+printf " | %-""$LINE""s |\n" "Flash: $RFS"
+printf " | %-""$LINE""s |\n" "Memory: $MEM"
 
-PROTO=$(uci -q get network.wan.proto)
-case $PROTO in
-qmi|ncm)
-	SEC=wan_4
-	;;
-*)
-	SEC=wan
-	;;
-esac
-WAN=$(ubus call network.interface status '{"interface":"'$SEC'"}' 2>/dev/null | jsonfilter -q -e "@['ipv4-address'][0].address")
-[ -z "$WAN" ] && WAN=$(uci -q -P /var/state get network.$SEC.ipaddr)
-[ -n "$WAN" ] && WAN="$WAN, proto: "$PROTO
+if [ -e /tmp/dhcp.leases ]; then
+	printf " | %-""$LINE""s |\n" "Leases: "$(awk 'END {print NR}' /tmp/dhcp.leases)
+fi
 
-printf " | %-"$LINE"s |\n" "Machine: $MACH"
-printf " | %-"$LINE"s |\n" "Uptime: $U"
-printf " | %-"$LINE"s |\n" "Load: $L"
-printf " | %-"$LINE"s |\n" "Flash: $RFS"
-printf " | %-"$LINE"s |\n" "Memory: $MEM"
-printf " | %-"$LINE"s |\n" "WAN: $WAN"
-printf " | %-"$LINE"s |\n" "LAN: $LAN"
+SEC=$(uci show network | awk -F[\.=] '/=interface/{print $2}')
+for i in $SEC; do
+	[ "x$i" = "xloopback" ] && continue
+	S=$i
+	PROTO=$(uci -q get network."$i".proto)
+	case $PROTO in
+	qmi|ncm)
+		S=$i"_4"
+		;;
+	esac
+	IP=$(ubus call network.interface status '{"interface":"'$S'"}' 2>/dev/null | jsonfilter -q -e "@['ipv4-address'][0].address")
+	[ -z "$IP" ] && IP=$(uci -q -P /var/state get network."$S".ipaddr)
+	printf " | %-""$LINE""s |\n" "$i: $PROTO, ${IP:-"?"}"
+done
 
-IFACES=$(uci -q show wireless | grep "device='radio" | cut -f2 -d. | sort)
-for i in $IFACES; do
-	SSID=$(uci -q get wireless.$i.ssid)
-	DEV=$(uci -q get wireless.$i.device)
-	OFF=$(uci -q get wireless.$DEV.disabled)
-	OFF2=$(uci -q get wireless.$i.disabled)
-	UP=$(ubus call network.wireless status '{"device":"'$DEV'"}' 2>/dev/null | jsonfilter -q -e @.*.up)
-	if [ -n "$SSID" ] && [ "x$OFF" != "x1" ] && [ "x$OFF2" != "x1" ] && [ "x$UP" == "xtrue" ]; then
-		MODE=$(uci -q -P /var/state get wireless.$i.mode)
-		CHANNEL=$(uci -q get wireless.$DEV.channel)
-		SEC1=$(echo $i | sed 's/\[/\\[/g;s/\]/\\]/g')
-		IFNAME=$(wifi status $DEV | grep -A 1 $SEC1 | awk '/ifname/ {gsub(/[",]/,"");print $2}')
-		[ -n "$IFNAME" ] && CNT=$(iw dev $IFNAME station dump 2>/dev/null | grep Station | wc -l)
-		printf " | %-"$LINE"s |\n" "$DEV: mode: $MODE, ssid: $SSID, channel: $CHANNEL, conn: ${CNT:-0}"
+SEC=$(uci -q show wireless | grep "device='radio" | cut -f2 -d. | sort)
+for i in $SEC; do
+	SSID=$(uci -q get wireless."$i".ssid)
+	DEV=$(uci -q get wireless."$i".device)
+	OFF=$(uci -q get wireless."$DEV".disabled)
+	OFF2=$(uci -q get wireless."$i".disabled)
+	STATUS=$(ubus call network.wireless status '{"device":"'"$DEV"'"}' 2>/dev/null)
+	UP=$(echo "$STATUS" | jsonfilter -q -e @.*.up)
+	if [ -n "$SSID" ] && [ "x$OFF" != "x1" ] && [ "x$OFF2" != "x1" ] && [ "x$UP" = "xtrue" ]; then
+		MODE=$(uci -q -P /var/state get wireless."$i".mode)
+		CHANNEL=$(uci -q get wireless."$DEV".channel)
+		NET=$(echo "$STATUS" | jsonfilter -e '@.*.interfaces[@.section="'"$i"'"].config.network[*]')
+		IFNAMES=$(echo "$STATUS" | jsonfilter -e '@.*.interfaces[@.section="'"$i"'"].ifname')
+		for j in $IFNAMES; do
+			CNT=$(iw dev $j station dump 2>/dev/null | grep -c Station)
+			printf " | %-""$LINE""s |\n" "$DEV: $NET, mode: $MODE, ssid: $SSID, channel: $CHANNEL, conn: ${CNT:-0}"
+		done
 	fi
 done
 
-echo " "$(for i in $(seq 2 $LINE1); do printf "-"; done)
+echo " "$(for i in $(seq 2 "$LINE1"); do printf "-"; done)
 
 ADDON=""
 for i in $(ls /etc/sysinfo.d/* 2>/dev/null); do
 	T=$($i)
 	if [ -n "$T" ]; then
-		printf " | %-"$LINE"s |\n" "$T"
+		printf " | %-""$LINE""s |\n" "$T"
 		ADDON="1"
 	fi
 done
 
 if [ -n "$ADDON" ]; then
-	echo " "$(for i in $(seq 2 $LINE1); do printf "-"; done)
+	echo " "$(for i in $(seq 2 "$LINE1"); do printf "-"; done)
 fi
 
 exit 0
