5f5a4b5cd99f1967670a1ecf63390f39e1f4a533
Cezary Jackiewicz <cezary@eko.one.pl>
2020-04-08 16:33:10 +0200

    easyconfig: adblock: fix for new version

--- easyconfig/files/www/easyconfig/easyconfig.js
+++ easyconfig/files/www/easyconfig/easyconfig.js
@@ -3423,8 +3423,8 @@ function showadblock() {
 			html += '<div class="slider round"></div>';
 			html += '</label>';
 			if (adblock_compressed_lists) {
-				html += '<span class="hidden-xs control-label labelleft">Wielkość: ' + adblock_lists[i].size + ', <a href="' + adblock_lists[i].desc + '" class="click" target="_blank">opis &rarr;</a></span>';
-				html += '<div class="visible-xs">Wielkość: ' + adblock_lists[i].size + ', <a href="' + adblock_lists[i].desc + '" class="click" target="_blank">opis &rarr;</a></div>';
+				html += '<span class="hidden-xs control-label labelleft">Kategoria: ' + adblock_lists[i].focus + ', wielkość: ' + adblock_lists[i].size + ', <a href="' + adblock_lists[i].descurl + '" class="click" target="_blank">opis &rarr;</a></span>';
+				html += '<div class="visible-xs">Kategoria: ' + adblock_lists[i].focus + ', wielkość: ' + adblock_lists[i].size + ', <a href="' + adblock_lists[i].descurl + '" class="click" target="_blank">opis &rarr;</a></div>';
 			} else {
 				html += '<span class="hidden-xs control-label labelleft">' + adblock_lists[i].desc + '</span>';
 				html += '<div class="visible-xs">' + adblock_lists[i].desc + '</div>';
@@ -3481,18 +3481,19 @@ function showadblock() {
 function saveadblock() {
 	var cmd = [];
 
-	cmd.push('uci set adblock.global.adb_enabled=' + (getValue("adblock_enabled") ? '1' : '0'));
-	cmd.push('uci set adblock.extra.adb_forcedns=' + (getValue("adblock_forcedns") ? '1' : '0'));
+	cmd.push('uci set adblock.global.adb_enabled=' + (getValue('adblock_enabled') ? '1' : '0'));
 	if (adblock_compressed_lists) {
+		cmd.push('uci set adblock.global.adb_forcedns=' + (getValue('adblock_forcedns') ? '1' : '0'));
 		cmd.push('uci -q del adblock.global.adb_sources');
 		for (var idx in adblock_lists) {
-			if (getValue("adblock_" + adblock_lists[idx].section)) {
+			if (getValue('adblock_' + adblock_lists[idx].section)) {
 				cmd.push('uci add_list adblock.global.adb_sources=' + adblock_lists[idx].section);
 			}
 		}
 	} else {
+		cmd.push('uci set adblock.extra.adb_forcedns=' + (getValue('adblock_forcedns') ? '1' : '0'));
 		for (var idx in adblock_lists) {
-			cmd.push('uci set adblock.' + adblock_lists[idx].section + '.enabled=' + (getValue("adblock_" + adblock_lists[idx].section) ? '1' : '0'));
+			cmd.push('uci set adblock.' + adblock_lists[idx].section + '.enabled=' + (getValue('adblock_' + adblock_lists[idx].section) ? '1' : '0'));
 		}
 	}
 	cmd.push('uci commit adblock');
