8a85e47f2e8f609667442205ee875fba9e1ddcfc
Cezary Jackiewicz <cezary@eko.one.pl>
2020-07-14 19:54:40 +0200

    easyconfig: little optimization

--- easyconfig/files/etc/uci-defaults/zz-easyconfig.sh
+++ easyconfig/files/etc/uci-defaults/zz-easyconfig.sh
@@ -2,8 +2,7 @@
 [ ! -e /www/index.html ] && ln -s /www/easyconfig.html /www/index.html
 uci set dhcp.@dnsmasq[0].logqueries='1'
 uci commit dhcp
-[ -e /etc/crontabs/root ] && sed -i "/easyconfig_wlanlogs/d" /etc/crontabs/root
+[ -e /etc/crontabs/root ] && sed -i "/easyconfig_wlanlogs/d;/easyconfig_statistics/d" /etc/crontabs/root
 echo "*/1 * * * * /usr/bin/easyconfig_wlanlogs.sh" >> /etc/crontabs/root
-[ -e /etc/crontabs/root ] && sed -i "/easyconfig_statistics/d" /etc/crontabs/root
 echo "*/1 * * * * /usr/bin/easyconfig_statistics.sh" >> /etc/crontabs/root
 exit 0
