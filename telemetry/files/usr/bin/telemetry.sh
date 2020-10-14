a245e8ae2a4fdf68d62c4d761e475d7d101bfc40
Cezary Jackiewicz <cezary@eko.one.pl>
2020-10-14 21:38:46 +0200

    telemetry: show info about all modules

--- telemetry/files/usr/bin/telemetry.sh
+++ telemetry/files/usr/bin/telemetry.sh
@@ -6,39 +6,45 @@
 DATA=""
 RES=/usr/lib/telemetry
 
+info() {
+	local config="$1"
+
+	config_get enabled "$config" enabled 1
+	config_get script "$config" script
+	if [ -e "$RES/$script" ]; then
+		value=$("$RES/$script" info)
+		config_get param "$config" param
+		printf "%-30s%-30s%-20s\n" "$value" "parameter: $param" "enabled: $enabled"
+	fi
+
+}
+
 handle_script() {
 	local config="$1"
 	local type="$2"
-	local info="$3"
 
 	config_get enabled "$config" enabled 1
 	[ "$enabled" -eq 1 ] || return
 
 	config_get script "$config" script
 	if [ -e "$RES/$script" ]; then
-		value=$("$RES/$script" $info)
+		value=$("$RES/$script")
 		config_get param "$config" param
-		if [ "x$info" = "xinfo" ]; then
-			printf "%-30s" "$value"
-			printf "%-30s" "parameter: $param"
-			printf "%-20s\n" "enabled: $enabled"
-		else
-			case "$type" in
-				"get" | "post")
-					[ "x$DATA" = "x" ] || DATA="${DATA}&"
-					DATA="${DATA}${param}=${value}"
-					;;
-				"mqtt")
-					if [ "x$DATA" = "x" ]; then
-						json_init
-					else
-						json_load "$(echo "$DATA")"
-					fi
-					json_add_string "${param}" "${value}"
-					DATA=$(json_dump)
-					;;
-			esac
-		fi
+		case "$type" in
+			"get" | "post")
+				[ "x$DATA" = "x" ] || DATA="${DATA}&"
+				DATA="${DATA}${param}=${value}"
+				;;
+			"mqtt")
+				if [ "x$DATA" = "x" ]; then
+					json_init
+				else
+					json_load "$(echo "$DATA")"
+				fi
+				json_add_string "${param}" "${value}"
+				DATA=$(json_dump)
+				;;
+		esac
 	fi
 }
 
@@ -47,8 +53,12 @@ parse_globals() {
 	config_get_bool enabled "$section" enabled 1
 	[ "$enabled" -eq 1 ] || exit 0
 	config_get type "$section" type ""
-	config_foreach handle_script script "$type" $2
-	[ "x$2" = "xinfo" ] && exit 0
+	if [ "x$2" = "xinfo" ]; then
+		config_foreach info script
+		exit 0
+	else
+		config_foreach handle_script script "$type"
+	fi
 	[ "x$2" = "xdebug" ] && DEBUG="echo " || DEBUG=""
 	case "$type" in
 		"get")
