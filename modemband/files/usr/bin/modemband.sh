fcee3ea8d7c70225c66154e6b672583f499249bc
Cezary Jackiewicz <cezary@eko.one.pl>
2022-01-18 07:05:36 +0100

    modemband: rewrite logic

--- modemband/files/usr/bin/modemband.sh
+++ modemband/files/usr/bin/modemband.sh
@@ -4,6 +4,56 @@
 # (c) 2022 Cezary Jackiewicz <cezary@eko.one.pl>
 #
 
+hextobands() {
+	HEX="$1"
+	LEN=${#HEX}
+	if [ $LEN -gt 18 ]; then
+		CNT=$((LEN - 16))
+		HHEX=${HEX:0:CNT}
+		HEX="0x"${HEX:CNT}
+	fi
+
+	for B in $(seq 0 63); do
+		POW=$((2 ** $B))
+		T=$((HEX&$POW))
+		[ "x$T" = "x$POW" ] && BANDS="${BANDS}$((B + 1)) "
+	done
+	if [ -n "$HHEX" ]; then
+		for B in $(seq 0 63); do
+			POW=$((2 ** $B))
+			T=$((HHEX&$POW))
+			[ "x$T" = "x$POW" ] && BANDS="${BANDS}$((B + 1 + 64)) "
+		done
+	fi
+	echo "$BANDS"
+}
+
+bandstohex() {
+	BANDS="$1"
+	SUM=0
+	HSUM=0
+	for BAND in $BANDS; do
+		case $BAND in
+			''|*[!0-9]*) continue ;;
+		esac
+		if [ $BAND -gt 64 ]; then
+			B=$((BAND - 1 - 64))
+			POW=$((2 ** $B))
+			HSUM=$((HSUM + POW))
+		else
+			B=$((BAND - 1))
+			POW=$((2 ** $B))
+			SUM=$((SUM + POW))
+		fi
+	done
+	if [ $HSUM -eq 0 ]; then
+		HEX=$(printf '%x' $SUM)
+	else
+		HEX=$(printf '%x%016x' $HSUM $SUM)
+	fi
+	echo "$HEX"
+}
+
 _DEVICE=""
 _DEFAULT_LTE_BANDS=""
 
@@ -66,7 +116,6 @@ case $1 in
 		setbands "$2"
 		;;
 	*)
-		echo ""
 		echo -n "Modem: "
 		getinfo
 		echo -n "Supported LTE bands: "
