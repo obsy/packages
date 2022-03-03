f5fc686c4904808bed56c64b2e0e341ed815195d
Cezary Jackiewicz <cezary@eko.one.pl>
2022-03-03 18:40:40 +0100

    modemband: allow custom modem port

--- modemband/files/usr/bin/modemband.sh
+++ modemband/files/usr/bin/modemband.sh
@@ -184,6 +184,11 @@ if [ -z "$_DEVICE" ]; then
 		echo "No supported modem was found, quitting..."
 	fi
 	exit 0
+else
+	_DEVICE1=$(uci -q get 3ginfo.@3ginfo[0].device)
+	if [ -n "$_DEVICE1" ]; then
+		_DEVICE=$_DEVICE1
+	fi
 fi
 if [ ! -e "$_DEVICE" ]; then
 	if [ "x$1" = "xjson" ]; then
