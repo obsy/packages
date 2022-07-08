d03241be6821f4c9b4e90a57d125814965ec347b
Cezary Jackiewicz <cezary@eko.one.pl>
2022-07-08 07:21:21 +0200

    easyconfig: clients: don't show last seen on empty data

--- easyconfig/files/www/easyconfig/easyconfig.js
+++ easyconfig/files/www/easyconfig/easyconfig.js
@@ -2808,7 +2808,11 @@ function hostinfo(id) {
 		html += createRowForModal('Adres IP', (host.ip == '' ? '-' : host.ip));
 	}
 	html += createRowForModal('Pierwszy raz widziany', formatDateTime(host.first_seen));
-	html += createRowForModal('Ostatni raz widziany', formatDateTime(host.last_seen) + '</span><span class="visible-xs oneline"></span><small><span>' + ' (' + formatDuration(parseInt((new Date() - new Date((host.last_seen).substring(0,4), (host.last_seen).substring(4,6) - 1, (host.last_seen).substring(6,8), (host.last_seen).substring(8,10), (host.last_seen).substring(10,12), (host.last_seen).substring(12,14)))/1000), false) + ' temu)' + '</span></small>');
+	if (host.last_seen == '-') {
+		html += createRowForModal('Ostatni raz widziany', '-');
+	} else {
+		html += createRowForModal('Ostatni raz widziany', formatDateTime(host.last_seen) + '</span><span class="visible-xs oneline"></span><small><span>' + ' (' + formatDuration(parseInt((new Date() - new Date((host.last_seen).substring(0,4), (host.last_seen).substring(4,6) - 1, (host.last_seen).substring(6,8), (host.last_seen).substring(8,10), (host.last_seen).substring(10,12), (host.last_seen).substring(12,14)))/1000), false) + ' temu)' + '</span></small>');
+	}
 	showMsg(html, false);
 }
 
