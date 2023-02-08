4b60f63fe15a83c6dbed41c3923b29cfd58ff764
Cezary Jackiewicz <cezary@eko.one.pl>
2023-02-08 06:37:44 +0100

    easyconfig: reorder at command

--- easyconfig/files/usr/share/easyconfig/modem/info.sh
+++ easyconfig/files/usr/share/easyconfig/modem/info.sh
@@ -25,7 +25,7 @@ fi
 
 O=""
 if [ -e /usr/bin/sms_tool ]; then
-	O=$(sms_tool -D -d $DEVICE at "AT+CSQ;+CPIN?;+COPS=3,0;+COPS?;+COPS=3,2;+COPS?;+CREG=2;+CREG?")
+	O=$(sms_tool -D -d $DEVICE at "AT+CPIN?;+CSQ;+COPS=3,0;+COPS?;+COPS=3,2;+COPS?;+CREG=2;+CREG?")
 else
 	O=$(gcom -d $DEVICE -s $RES/info.gcom 2>/dev/null)
 fi
