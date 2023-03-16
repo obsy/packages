8302c4714d72f2fcddf0014abdbaa3ad40be5f15
Cezary Jackiewicz <cezary@eko.one.pl>
2023-03-16 06:45:21 +0100

    easyconfig: setapn: read profile from config

--- easyconfig/files/usr/bin/easyconfig_setapn.sh
+++ easyconfig/files/usr/bin/easyconfig_setapn.sh
@@ -4,8 +4,9 @@ MODEM=$1
 [ -z "$MODEM" ] && exit 0
 [ -e "$MODEM" ] || exit 0
 [ -z $(command -v chat) ] && exit 0
-APN=$(uci -q get network.wan.apn)
-[ -z "$APN" ] && exit 0
+
+PROFILE=$(uci -q get network.wan.profile)
+[ -z "$PROFILE" ] && PROFILE=1
 
 T=$(uci -q get network.wan.pdptype | awk '{print tolower($0)}')
 case "$T" in
@@ -14,6 +15,9 @@ case "$T" in
 	*) PDPTYPE="IP";;
 esac
 
-chat -t 3 -e '' 'AT+CGDCONT=1,"'$PDPTYPE'","'$APN'"' OK >> $MODEM < $MODEM
+APN=$(uci -q get network.wan.apn)
+[ -z "$APN" ] && exit 0
+
+chat -t 3 -e '' 'AT+CGDCONT='$PROFILE',"'$PDPTYPE'","'$APN'"' OK >> $MODEM < $MODEM
 
 exit 0
