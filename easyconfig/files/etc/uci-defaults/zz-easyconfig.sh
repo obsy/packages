8ed3f019952dd4a5a942ff62ea26b1c84fec369a
Cezary Jackiewicz <cezary@eko.one.pl>
2023-04-13 17:47:12 +0200

    easyconfig: allow to preserve logqueries flag

--- easyconfig/files/etc/uci-defaults/zz-easyconfig.sh
+++ easyconfig/files/etc/uci-defaults/zz-easyconfig.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 [ ! -e /www/index.html ] && ln -s /www/easyconfig.html /www/index.html
-uci set dhcp.@dnsmasq[0].logqueries='1'
-uci commit dhcp
 [ -e /etc/crontabs/root ] && sed -i "/easyconfig_wlanlogs/d;/easyconfig_logs/d;/easyconfig_statistics/d" /etc/crontabs/root
 echo "*/1 * * * * /usr/bin/easyconfig_logs.sh" >> /etc/crontabs/root
 echo "*/1 * * * * /usr/bin/easyconfig_statistics.sh" >> /etc/crontabs/root
