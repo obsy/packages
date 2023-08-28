211caa9681bf8b00218fb6ec35bb1494e29748ef
Cezary Jackiewicz <cezary@eko.one.pl>
2023-08-28 07:33:04 +0200

    backupandrestore: fix for multiple entries in LuCI configuration

--- backupandrestore/files/etc/uci-defaults/backupandrestore.sh
+++ backupandrestore/files/etc/uci-defaults/backupandrestore.sh
@@ -2,12 +2,21 @@
 
 [ -e /etc/config/luci ] || exit 0
 
-uci add luci command
-uci set luci.@command[-1].name='Instalacja pakietów użytkownka'
-uci set luci.@command[-1].command='/usr/bin/restorepkgslist.sh verbose'
-uci add luci command
-uci set luci.@command[-1].name='Archiwizacja listy pakietów użytkownika'
-uci set luci.@command[-1].command='/usr/bin/backuppkgslist.sh verbose'
-uci commit
+if uci show luci | grep -q restorepkgslist.sh; then
+	true
+else
+	uci add luci command
+	uci set luci.@command[-1].name='Instalacja pakietów użytkownka'
+	uci set luci.@command[-1].command='/usr/bin/restorepkgslist.sh verbose'
+fi
+
+if uci show luci | grep -q backuppkgslist.sh; then
+	true
+else
+	uci add luci command
+	uci set luci.@command[-1].name='Archiwizacja listy pakietów użytkownika'
+	uci set luci.@command[-1].command='/usr/bin/backuppkgslist.sh verbose'
+fi
+uci commit luci
 
 exit 0
