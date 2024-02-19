3ce346b086000caa7fb2e2cb7eae22e9ed8cb140
Cezary Jackiewicz <cezary@eko.one.pl>
2024-02-19 06:30:56 +0100

    easyconfig: clients: show link to bandwidth graph on main page

--- easyconfig/files/www/easyconfig/easyconfig.js
+++ easyconfig/files/www/easyconfig/easyconfig.js
@@ -3314,7 +3314,7 @@ function clientscallback(sortby) {
 					html += '<div class="col-xs-2"></div>';
 				} else {
 					html += 'bezprzewodowo<br>' + (sorted[idx].band == 2 ? '2.4' : sorted[idx].band) + ' GHz</div>';
-					html += '<div class="col-xs-2 hidden-xs"><span title="wysłano">&uarr;&nbsp;' + bytesToSize(sorted[idx].tx) + '</span><br><span title="pobrano">&darr;&nbsp;' + bytesToSize(sorted[idx].rx) + '</span></div>';
+					html += '<div class="col-xs-2 hidden-xs"><span class="click" onclick="showbandwidth(\'' + sorted[idx].mac + '\');" title="wysłano">&uarr;&nbsp;' + bytesToSize(sorted[idx].tx) + '</span><br><span class="click" onclick="showbandwidth(\'' + sorted[idx].mac + '\');" title="pobrano">&darr;&nbsp;' + bytesToSize(sorted[idx].rx) + '</span></div>';
 				}
 				html += '<div class="col-xs-1 hidden-xs text-right"><span class="click" title="menu" onclick="hostmenu(' + sorted[idx].id + ');"><i data-feather="more-vertical"></i></span></div>';
 
