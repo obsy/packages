3a78345bf246c6f5b555cd219254c9def75886c3
Cezary Jackiewicz <cezary@eko.one.pl>
2024-03-11 06:27:57 +0100

    easyconfig: clean up

--- easyconfig/files/etc/uci-defaults/zz-easyconfig.sh
+++ easyconfig/files/etc/uci-defaults/zz-easyconfig.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 [ ! -e /www/index.html ] && ln -s /www/easyconfig.html /www/index.html
-[ -e /etc/crontabs/root ] && sed -i "/easyconfig_wlanlogs/d;/easyconfig_logs/d;/easyconfig_statistics/d" /etc/crontabs/root
+[ -e /etc/crontabs/root ] && sed -i "/easyconfig_logs/d;/easyconfig_statistics/d" /etc/crontabs/root
 echo "*/1 * * * * /usr/bin/easyconfig_logs.sh" >> /etc/crontabs/root
 echo "*/1 * * * * /usr/bin/easyconfig_statistics.sh" >> /etc/crontabs/root
 exit 0
