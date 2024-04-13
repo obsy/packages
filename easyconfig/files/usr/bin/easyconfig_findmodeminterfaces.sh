d2b620598ef47def78d8c23192e3492078ada688
Cezary Jackiewicz <cezary@eko.one.pl>
2024-04-13 22:36:35 +0200

    easyconfig: correct slot name

--- easyconfig/files/usr/bin/easyconfig_findmodeminterfaces.sh
+++ easyconfig/files/usr/bin/easyconfig_findmodeminterfaces.sh
@@ -14,7 +14,7 @@ findinterfaces() {
 
 case "$(cat /tmp/sysinfo/board_name)" in
 alfa-network,quad-e4g)
-	for i in "m2      :usb2/2-1" "minipci0:usb1/1-2/1-2.4" "minipci1:usb1/1-2/1-2.3" "minipci2:usb1/1-2/1-2.2" "usb     :usb1/1-2/1-2.1"; do
+	for i in "m2       :usb2/2-1" "minipcie0:usb1/1-2/1-2.4" "minipcie1:usb1/1-2/1-2.3" "minipcie2:usb1/1-2/1-2.2" "usb      :usb1/1-2/1-2.1"; do
 		T1=$(echo "$i" | cut -f1 -d:)
 		T2=$(echo "$i" | cut -f2 -d:)
 		findinterfaces "/sys/devices/platform/1e1c0000.xhci/$T2" "$T1 "
