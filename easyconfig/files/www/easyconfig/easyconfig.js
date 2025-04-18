560e1ca894e8bf76c928c0a9cf77afd9105757be
Cezary Jackiewicz <cezary@eko.one.pl>
2025-04-18 07:10:14 +0200

    easyconfig: add more units

--- easyconfig/files/www/easyconfig/easyconfig.js
+++ easyconfig/files/www/easyconfig/easyconfig.js
@@ -3726,7 +3726,7 @@ wifigraph = {
 /*****************************************************************************/
 
 function bytesToSize(bytes) {
-	var sizes = ['', 'KiB', 'MiB', 'GiB', 'TiB'];
+	var sizes = ['', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
 	if (bytes == 0) return '0';
 	var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
 	var dm = 0;
